1e382322f3ce6d08712a13881bbafffa027071be
Meixu Song <songmeixu@lixiang.com>
2024-01-05 16:29:57 +0800

    support macos/arm

--- cmake/onnxruntime.cmake
+++ cmake/onnxruntime.cmake
@@ -1,16 +1,22 @@
 function(download_onnxruntime)
   include(FetchContent)
 
-  # Please also change ../pack-for-embedded-systems.sh\
   set(ONNX_VERSION "1.15.1")
-  if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
-    # official build release: 15MB lib
-    # set(onnxruntime_URL "https://github.com/microsoft/onnxruntime/releases/download/v${ONNX_VERSION}/onnxruntime-linux-aarch64-${ONNX_VERSION}.tgz")
-    # set(onnxruntime_HASH "SHA256=85272e75d8dd841138de4b774a9672ea93c1be108d96038c6c34a62d7f976aee")
-    # custom build: 2MB lib
-    set(onnxruntime_URL "${PROJECT_SOURCE_DIR}/cmake/onnxruntime-linux-aarch64-${ONNX_VERSION}.tgz")
-    set(onnxruntime_HASH "SHA256=aa938bb9211d28080b3e8f5e25b50b87257ec201301b491f5296595a9b755392")
+
+  # Check if the system processor is aarch64 (ARM64)
+  if(CMAKE_SYSTEM_PROCESSOR MATCHES "arm64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
+    # Check if the system name is Darwin (macOS/OSX)
+    if (CMAKE_SYSTEM_NAME MATCHES "Darwin")
+      # macOS/OSX arm64 specific settings
+      set(onnxruntime_URL "https://github.com/microsoft/onnxruntime/releases/download/v${ONNX_VERSION}/onnxruntime-osx-arm64-${ONNX_VERSION}.tgz")
+      set(onnxruntime_HASH "SHA256=df97832fc7907c6677a6da437f92339d84a462becb74b1d65217fcb859ee9460")
+    else()
+      # Linux ARM64 settings
+      set(onnxruntime_URL "${PROJECT_SOURCE_DIR}/cmake/onnxruntime-linux-aarch64-${ONNX_VERSION}.tgz")
+      set(onnxruntime_HASH "SHA256=aa938bb9211d28080b3e8f5e25b50b87257ec201301b491f5296595a9b755392")
+    endif()
   else()
+    # General settings for systems other than ARM64
     set(onnxruntime_URL "https://github.com/microsoft/onnxruntime/releases/download/v${ONNX_VERSION}/onnxruntime-linux-x64-${ONNX_VERSION}.tgz")
     set(onnxruntime_HASH "SHA256=5492f9065f87538a286fb04c8542e9ff7950abb2ea6f8c24993a940006787d87")
   endif()
@@ -22,7 +28,7 @@ function(download_onnxruntime)
 
   FetchContent_MakeAvailable(onnxruntime)
 
-  message(STATUS "onnxruntime is downloaded to ${onnxruntime_SOURCE_DIR}")
+  message(STATUS "onnxruntime (${onnxruntime_URL}) is downloaded to ${onnxruntime_SOURCE_DIR}")
   message(STATUS "onnxruntime's binary dir is ${onnxruntime_BINARY_DIR}")
 
   # add_subdirectory(${onnxruntime_SOURCE_DIR} ${onnxruntime_BINARY_DIR})
